e2d4ec106cfd4797d3272f9924ea3dfb6c99d86b
Ramsensei <ramsensei@estudiantec.cr>
2024-09-30 21:34:04 -0600

    comida

--- biquad.cpp
+++ biquad.cpp
@@ -85,25 +85,23 @@ bool biquad::process(jack_nframes_t nframes,
     float volume_intensity = this->volume_controller_prt == nullptr ? 1 : this->volume_controller_prt->get_volume_intesity();
     const sample_t *const end_ptr = in + nframes;
     const sample_t *in_ptr = in;
-    const sample_t *in_ptr1 = in;
-    const sample_t *in_ptr2 = in;
 
     sample_t *out_ptr = out;
     sample_t y0, y1, y2, y3, y4, y5, y6, y7 = 0; 
 
-    for (;in_ptr != end_ptr;){
-        y0 =  (this->b0 * *(in_ptr++)  +  this->b1 * x_past_1 + this->b2 * x_past_2 - this->a1 * y_past_1 - this->a2 * y_past_2);
-        y1 =  (this->b0 * *(in_ptr++)  +  this->b1 * *(in_ptr1++) + this->b2 * x_past_1 - this->a1 * y0 - this->a2 * y_past_1);
-        y2 =  (this->b0 * *(in_ptr++)  +  this->b1 * *(in_ptr1++) + this->b2 * *(in_ptr2++) - this->a1 * y1 - this->a2 * y0);
-        y3 =  (this->b0 * *(in_ptr++)  +  this->b1 * *(in_ptr1++) + this->b2 * *(in_ptr2++) - this->a1 * y2 - this->a2 * y1);
-        y4 =  (this->b0 * *(in_ptr++)  +  this->b1 * *(in_ptr1++) + this->b2 * *(in_ptr2++) - this->a1 * y3 - this->a2 * y2);
-        y5 =  (this->b0 * *(in_ptr++)  +  this->b1 * *(in_ptr1++) + this->b2 * *(in_ptr2++) - this->a1 * y4 - this->a2 * y3);
-        y6 =  (this->b0 * *(in_ptr++)  +  this->b1 * *(in_ptr1++) + this->b2 * *(in_ptr2++) - this->a1 * y5 - this->a2 * y4);
-        y7 =  (this->b0 * *(in_ptr)  +  this->b1 * *(in_ptr1) + this->b2 * *(in_ptr2++) - this->a1 * y6 - this->a2 * y5);
+    while (in_ptr != end_ptr) {
+        y0 =  (this->b0 * *(in_ptr)  +  this->b1 * x_past_1 + this->b2 * x_past_2 - this->a1 * y_past_1 - this->a2 * y_past_2);
+        y1 =  (this->b0 * *(in_ptr + 1)  +  this->b1 * *(in_ptr) + this->b2 * x_past_1 - this->a1 * y0 - this->a2 * y_past_1);
+        y2 =  (this->b0 * *(in_ptr + 2)  +  this->b1 * *(in_ptr + 1) + this->b2 * *(in_ptr) - this->a1 * y1 - this->a2 * y0);
+        y3 =  (this->b0 * *(in_ptr + 3)  +  this->b1 * *(in_ptr + 2) + this->b2 * *(in_ptr + 1) - this->a1 * y2 - this->a2 * y1);
+        y4 =  (this->b0 * *(in_ptr + 4)  +  this->b1 * *(in_ptr + 3) + this->b2 * *(in_ptr + 2) - this->a1 * y3 - this->a2 * y2);
+        y5 =  (this->b0 * *(in_ptr + 5)  +  this->b1 * *(in_ptr + 4) + this->b2 * *(in_ptr + 3) - this->a1 * y4 - this->a2 * y3);
+        y6 =  (this->b0 * *(in_ptr + 6)  +  this->b1 * *(in_ptr + 5) + this->b2 * *(in_ptr + 4) - this->a1 * y5 - this->a2 * y4);
+        y7 =  (this->b0 * *(in_ptr + 7)  +  this->b1 * *(in_ptr + 6) + this->b2 * *(in_ptr + 5) - this->a1 * y6 - this->a2 * y5);
 
         // update states
-        this->x_past_2 = *in_ptr1;
-        this->x_past_1 = *in_ptr;
+        this->x_past_2 = *(in_ptr + 6);
+        this->x_past_1 = *(in_ptr + 7);
         this->y_past_2 = y6;
         this->y_past_1 = y7;
 
@@ -116,9 +114,8 @@ bool biquad::process(jack_nframes_t nframes,
         *(out_ptr++) = volume_intensity * y6;
         *(out_ptr++) = volume_intensity * y7;
 
-        // update pointers
-        ++in_ptr;
-        ++in_ptr1;
+        // update pointer
+        in_ptr += 8;
     }
     
   return true;
